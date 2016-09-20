0f8eeb730dff16e5959c8347d35578b3ee58a5d5
SeiyaKurokome <seiyakurokome@Seiya-no-MacBook-Air.local>
2016-09-20 21:18:45 +0900

    first commit

---
+++ TodoApp/TodoApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TodoApp
+//
+//  Created by Seiya Kurokome on 2016/09/20.
+//  Copyright © 2016年 Seiya Kurokome. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
